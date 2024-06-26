5cc4c2b4331ea9ab2076dabbe97d922d0396e70a
xRain <xrain@simcu.com>
2024-06-27 05:05:00 +0800

    temp

--- projects/simapi/src/public-api.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Public API Surface of simapi
- */
-export * from './lib/simapi.service';
-export * from './lib/simapi-auth.service';
-export * from './lib/simapi-oidc.service';
-export * from './lib/simapi.module';
-export * from './lib/simapi-config.service';
